78b66ce206538fbc8a6cdae88ebdf5a30fd3c95f
Timothy Nguyen <timothytnguyen@ucsb.edu>
2025-05-01 22:33:08 -0700

    Create UCSBOrg pages and tests

--- frontend/src/main/pages/UCSBOrganizations/UCSBOrganizationsCreatePage.js
+++ frontend/src/main/pages/UCSBOrganizations/UCSBOrganizationsCreatePage.js
@@ -12,7 +12,7 @@ export default function UCSBOrganizationsCreatePage({ storybook = false }) {
       orgCode: ucsborganization.orgCode,
       orgTranslationShort: ucsborganization.orgTranslationShort,
       orgTranslation: ucsborganization.orgTranslation,
-      inactive: ucsborganization.inactive
+      inactive: ucsborganization.inactive,
     },
   });
 
@@ -42,7 +42,7 @@ export default function UCSBOrganizationsCreatePage({ storybook = false }) {
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Create New Restaurant</h1>
+        <h1>Create New UCSB Organization</h1>
         <UCSBOrganizationsForm submitAction={onSubmit} />
       </div>
     </BasicLayout>
