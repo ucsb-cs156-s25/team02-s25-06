829d78922faeb1f18def8ff97e006e6be9734761
yarwinliu <yarwinliu@ucsb.edu>
2025-05-02 17:39:43 -0700

    yl - navbar

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -72,6 +72,9 @@ export default function AppNavbar({
                   <Nav.Link as={Link} to="/helpRequests">
                     Help Requests
                   </Nav.Link>
+                  <Nav.Link as={Link} to="/menuItemReview">
+                    Menu Item Reviews
+                  </Nav.Link>
                 </>
               ) : (
                 <></>
