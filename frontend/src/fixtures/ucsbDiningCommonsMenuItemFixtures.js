04d60c2f5cc7d0597e3323ab14b531bc32ed3630
Saeed Arellano <saeedarellano@ucsb.edu>
2025-05-04 11:30:26 -0700

    create fixtures for ucsbdiningcommonsmenuitem

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,34 @@
+const ucsbDiningCommonsMenuItemFixtures = {
+    oneucsbDiningCommonsMenuItem: [
+        {
+            "id": 8,
+            "diningCommonsCode": "ortega",
+            "name": "Baked Pesto Pasta with Chicken",
+            "station": "Entree Specials"
+        },
+    ],
+  
+    threeucsbDiningCommonsMenuItem: [
+        {
+          "id": 8,
+          "diningCommonsCode": "ortega",
+          "name": "Baked Pesto Pasta with Chicken",
+          "station": "Entree Specials"
+        },
+        {
+          "id": 9,
+          "diningCommonsCode": "ortega",
+          "name": "Tofu Banh Mi Sandwich (v)",
+          "station": "Entree Specials"
+        },
+        {
+          "id": 10,
+          "diningCommonsCode": "ortega",
+          "name": "Chicken Caesar Salad",
+          "station": "Entrees"
+        }
+      ]
+  };
+  
+  export { ucsbDiningCommonsMenuItemFixtures };
+  
