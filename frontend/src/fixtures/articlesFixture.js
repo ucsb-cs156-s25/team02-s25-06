19c3da3af1276ab860704a62b1239003afe6ba9d
Sanjana Jilla <sjilla@ucsb.edu>
2025-05-04 14:24:00 -0700

    articles fixture commit

---
+++ frontend/src/fixtures/articlesFixture.js
@@ -0,0 +1,38 @@
+const articlesFixture = {
+  oneArticle: {
+    "id": 1,
+    "title": "Team 02 Frontend Fixtures ",
+    "url": "https://www.youtube.com/watch?v=u-jJ-ZWFpeE",
+    "explanation": "This explains step 3 of team02. ",
+    "email": "sjilla@gmail.com",
+    "dateAdded": "2025-05-04T19:46:01"
+  },
+  threeArticles: [
+    {
+      "id": 1,
+      "title": "Team 02 Frontend Fixtures ",
+      "url": "https://www.youtube.com/watch?v=u-jJ-ZWFpeE",
+      "explanation": "This explains step 3 of team02. ",
+      "email": "sjilla@gmail.com",
+      "dateAdded": "2025-05-04T19:46:01"
+    },
+    {
+      "id": 2,
+      "title": "Article 2 Title",
+      "url": "https://github.com/orgs/ucsb-cs156-s25/projects/31/views/1",
+      "explanation": "This is a sample article",
+      "email": "sjilla@ucsb.edu",
+      "dateAdded": "2025-05-04T19:46:01"
+    },
+    {
+      "id": 3,
+      "title": "Article 3 Title",
+      "url": "https://github.com/ucsb-cs156-s25/team02-s25-06/issues/54",
+      "explanation": "This is a sample article again",
+      "email": "sjilla@ucsb.edu",
+      "dateAdded": "2025-05-04T19:46:01"
+    },
+  ],
+};
+
+export { articlesFixtures };
