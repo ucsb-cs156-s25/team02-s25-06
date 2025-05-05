22038949987ea17ff672662cd934acf1071f73cd
aryanchopra1802 <aryanchopra@ucsb.edu>
2025-05-05 11:06:36 -0700

    Added fixtures in the Rec Rec table, will be used for storybook, tec. laster on

---
+++ frontend/src/fixtures/recommendationrequestFixtures.js
@@ -0,0 +1,42 @@
+const recommendationrequestFixtures = {
+  oneRecommendationRequest: {
+    "id": 1,
+    "requesterEmail": "blah",
+    "professorEmail": "blah",
+    "explanation": "blah",
+    "dateRequested": "2024-01-01",
+    "dateNeeded": "2025-01-01",
+    "done": true
+  },
+  threeDates: [
+    {
+      "id": 1,
+      "requesterEmail": "blah",
+      "professorEmail": "blah",
+      "explanation": "blah",
+      "dateRequested": "2024-01-01",
+      "dateNeeded": "2025-01-01",
+      "done": true
+    },
+    {
+      "id": 2,
+      "requesterEmail": "test_requester@test.com",
+      "professorEmail": "test_professor@test.com",
+      "explanation": "test explanation updated",
+      "dateRequested": "2020-01-01",
+      "dateNeeded": "2022-02-02",
+      "done": true
+    },
+    {
+      "id": 3,
+      "requesterEmail": "test2_requester@test.com",
+      "professorEmail": "test2_professor@test.com",
+      "explanation": "test2 explanation",
+      "dateRequested": "2000-01-01",
+      "dateNeeded": "2002-02-02",
+      "done": true
+    }
+  ]
+};
+
+export { recommendationrequestFixtures };
