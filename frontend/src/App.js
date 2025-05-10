0cbc06e2c19836747f978c08d668ceadd9336908
aryanchopra1802 <aryanchopra@ucsb.edu>
2025-05-09 19:54:08 -0700

    test commit fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -70,7 +70,7 @@ function App() {
             />
           </>
         )}
-        
+
         {hasRole(currentUser, "ROLE_USER") && (
           <>
             <Route exact path="/article" element={<ArticleIndexPage />} />
